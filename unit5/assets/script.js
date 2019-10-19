28fd784fef90f9c82f77096d57cc2985b4a6b35b
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-10-19 17:24:08 -0500

    updated HTML and JS files

--- unit5/assets/script.js
+++ unit5/assets/script.js
@@ -1,23 +1,37 @@
-// Date 
-var now = moment();
-$("#todayDate").text("Today is " + now.format('MMMM Do, YYYY'));
+console.log("test")
 
-// Arrays
-// var hoursArray = [9,10,11,12,13,14,15,16,17];
-// var hour = moment().hour();
+$(".saveBtn").on("click",function(){
+    var value = $(this).siblings(".description").val();
+    var time= $(this).parent().attr("id");
+    localStorage.setItem(time,value);
+ })
+ 
+ 
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));​
 
-// changeColor.function() 
-//     if(hoursArray[i] === hour) {
-//         $(".plannerText").addClass("present")
-//     }
 
-
-
-
-//row count
-
-// var rowCount = [0,1,2,3,4,5,6]
-
-
-// $(".row").append($(".row"));
-
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  var currentHour = moment().hours();
+ 
+  $(".time-block").each(function( index ) {
+      console.log( index + ": " + $( this ) );
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      console.log(blockHour);
+      if(blockHour< currentHour){
+     $(this).addClass(".past")
+      }
+      else if (blockHour == currentHour) {
+          $(this).addClass(".present")
+      }
+      else {
+        $(this).addClass(".future")
+      };
+    });
