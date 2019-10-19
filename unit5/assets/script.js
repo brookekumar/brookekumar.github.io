f04c7fda42f0b6f7d3ab0c2692e74fd9fa2b5fb6
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-10-19 10:04:29 -0500

    added rows to HTML and date via JS

---
+++ unit5/assets/script.js
@@ -0,0 +1,23 @@
+// Date 
+var now = moment();
+$("#todayDate").text("Today is " + now.format('MMMM Do, YYYY'));
+
+// Arrays
+// var hoursArray = [9,10,11,12,13,14,15,16,17];
+// var hour = moment().hour();
+
+// changeColor.function() 
+//     if(hoursArray[i] === hour) {
+//         $(".plannerText").addClass("present")
+//     }
+
+
+
+
+//row count
+
+// var rowCount = [0,1,2,3,4,5,6]
+
+
+// $(".row").append($(".row"));
+
