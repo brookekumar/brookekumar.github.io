61e4679c256ce047a400920fcefe036afcaeae95
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-10-20 15:40:26 -0500

    updated JS HTML and CSS files

--- unit5/assets/script.js
+++ unit5/assets/script.js
@@ -8,14 +8,14 @@ $(".saveBtn").on("click",function(){
  
  
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));​
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
@@ -26,12 +26,12 @@ $("#hour-17 .description").val(localStorage.getItem("hour-17"));​
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
       console.log(blockHour);
       if(blockHour< currentHour){
-     $(this).addClass(".past")
+     $(this).addClass("past")
       }
       else if (blockHour == currentHour) {
-          $(this).addClass(".present")
+          $(this).addClass("present")
       }
       else {
-        $(this).addClass(".future")
+        $(this).addClass("future")
       };
     });
