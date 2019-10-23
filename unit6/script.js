6ce5595e74293c555f37a1df78f6c65193f89459
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-10-22 22:36:23 -0500

    changed JS and HTML to append cards to HTML dynamically

--- unit6/script.js
+++ unit6/script.js
@@ -1,5 +1,5 @@
-var currentURL = "http://api.openweathermap.org/data/2.5/weather?q={cityname}&APPID=e8df93d43208d9dd2e275275fb8aa604"
-var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q={cityname}&APPID=e8df93d43208d9dd2e275275fb8aa604"
+var currentURL = "http://api.openweathermap.org/data/2.5/weather?q={cityname}&APPID=e8df93d43208d9dd2e275275fb8aa604";
+var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q={cityname}&APPID=e8df93d43208d9dd2e275275fb8aa604";
 
 searchHistory = localStorage.getItem("searchHistoryKey");
     if (searchHistory == null) {
@@ -32,14 +32,28 @@ function search(city){
         url: fiveDays,
         method: "GET"
     })
-        .then(function(response) {
+        .then(function(response,data) {
         var parsedResponse = parseFiveDay(response);
         console.log(JSON.stringify(parsedResponse,2))
         });
-    
+
+        var date = $("<h3>").addClass("cardDate").text(data.name + new Date().toLocaleDateString());
+        var card = $("<div>").addClass("cardDiv");
+        var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        var temp = $("<p>").addClass("cardText").text("Temperature" + data.main.temp + "F");
+        var humidity = $("<p>").addClass("cardText").text("Humidity" + data.main.humidity + "%");
+        var cardBody = $("<div>").addClass("cardBody");
+
+        cardBody.append(date,img,temp,humidity);
+        card.append(cardBody);
+         
     searchHistory.unshift(city);
 
     localStorage.setItem("searchHistoryKey", JSON.stringify(searchHistory));
+    
+    var prepend = $(".list-group").prepend("<button>"+ searchHistory[0] +"</button>")
+    
+    localStorage.getItem("prepend");
 }
 function parseFiveDay(response){
     var output = [];
@@ -49,3 +63,5 @@ function parseFiveDay(response){
 return output;
 
 }
+
+//add a fail function 
