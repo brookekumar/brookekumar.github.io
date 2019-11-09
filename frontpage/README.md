599e597ad0b7fa4509688c3a20bd45536bb6702d
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-11-09 11:55:56 -0600

    updated all project document formatting adnd moved README

--- frontpage/README.md
+++ frontpage/README.md
@@ -1,2 +1,28 @@
-# brookekumar.github.io
-Brooke Kumar Github page
+#brookekumar.github.io
+
+[Brooke Kumar via Github](https://brookekumar.github.io/)
+
+Included: 
+1. name
+2. email
+3. linkedin and angellist pages
+4. resume (including phone, email, address)
+
+#Git index and contact pages are mobile-first and responsive 
+#PDF always opens on new tab - depending on user-s browser settings
+
+##Screenshots: 
+
+###Index page
+
+![Full-size index page][frontpage/images/index_full.png]
+
+![Responsive index page][frontpage/images/index_responsive.png]
+
+###Contact page
+
+![Full-size contact page][frontpage/images/contact_full.png]
+
+![Responsive contact page][frontpage/images/contact_responsive.png]
+
+*Contact page does not link to email*
--- unit4/assets/javascript.js
+++ unit4/assets/javascript.js
@@ -1,157 +1,186 @@
-$(document).ready(function () {
-    // ---variables---
-
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
-    var unansweredQuestions = 0;
-    var timeRemaining = 5;
-    var intervalID;
-    var indexQandA = 0; //index to load a different question each round without the game reset or screen refresh
-    var answered = false; //variable to stop the timer if user has clicked an answer
-    var correct;
-    var triviaGame = [{ 
-        question: "A Unicorn is a...",
-        answer: ["Mythical animal","Horse with a horn","Toddler with a cup on his head","Actually an above-water narwhal"],
-        correct: "0",
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+// sound effects
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+function startQuiz() {
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+  // show starting time
+  timerEl.textContent = time;
+  getQuestion();
+}
+function getQuestion() {
+  // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
+  // loop over choices
+  currentQuestion.choices.forEach(function(choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = i + 1 + ". " + choice;
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
+function questionClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
+    if (time < 0) {
+      time = 0;
+    }
+    // display new time on page
+    timerEl.textContent = time;
+    // play "wrong" sound effect
+    sfxWrong.play();
+    feedbackEl.textContent = "Wrong!";
+  } else {
+    // play "right" sound effect
+    sfxRight.play();
+    feedbackEl.textContent = "Correct!";
+  }
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+  // move to next question
+  currentQuestionIndex++;
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
+  // show end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+  // show final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+  // hide questions section
+  questionsEl.setAttribute("class", "hide");
+}
+function clockTick() {
+  // update time
+  time--;
+  timerEl.textContent = time;
+  // check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+  // make sure value wasn't empty
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    // redirect to next page
+    window.location.href = "highscores.html";
+  }
+}
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+initialsEl.onkeyup = checkForEnter;
+    var questions = [
+    { 
+        title: "A Unicorn is a...",
+        choices: ["Mythical animal","Horse with a horn","Toddler with a cup on his head","Actually an above-water narwhal"],
+        answer: "0",
     },
 
     { 
-        prompt: "If a Unicorn and a Pegasus mate the babies might...",
-        answer: ["Be human","Have to move to the mother's country of origin","Fly","Not fit in at school"],
-        correct: "2",
+        title: "If a Unicorn and a Pegasus mate the babies might...",
+        choices: ["Be human","Have to move to the mother's country of origin","Fly","Not fit in at school"],
+        answer: "2",
     },
 
     { 
-        prompt: "According to Jewish legend, the Unicorn can easily kill...",
-        answer: ["A bank robber","An elephant","A peacock","Bad vibes"],
-        correct: "1",
+        title: "According to Jewish legend, the Unicorn can easily kill...",
+        choices: ["A bank robber","An elephant","A peacock","Bad vibes"],
+        answer: "1",
     },
 
     { 
-        prompt: "A baby unicorn is called a...",
-        answer: ["Shirley","Modern miracle","Sparkle","(une) petite chevalle de corne sans pour réalité"],
-        correct: "2",
+        title: "A baby unicorn is called a...",
+        choices: ["Shirley","Modern miracle","Sparkle","(une) petite chevalle de corne sans pour réalité"],
+        answer: "2",
     },
 
     { 
-        prompt: "The fossil remains of a real unicorn or the 'Elasmotherium sibiricu' dating back 29,000 years ago were found in which country?",
-        answer: ["North America","India","ur mom","Kazakhstan"],
-        correct: "3"
+        title: "The fossil remains of a real unicorn or the 'Elasmotherium sibiricu' dating back 29,000 years ago were found in which country?",
+        choices: ["North America","India","ur mom","Kazakhstan"],
+        answer: "3"
     },
 ];
-    // ---functions---
-
-
-    function startGame() {
-        console.log("game has begun");
-        $('.start-button').remove();
-        $('.instructions').remove();
-        correctAnswers = 0;
-        incorrectAnswers = 0;
-        unansweredQuestions = 0;
-        loadQandA();
-    }
-
-    function loadQandA() {
-        answered = false; // will allow timeRemaining to be pushed back to <h5> after round reset....else statement in function timer()
-        timeRemaining = 3;
-        intervalID = setInterval(timer, 1000);
-        if (answered === false) {
-            timer();
-        }
-        correct = triviaGame[indexQandA].correct;
-        var question = triviaGame[indexQandA].question;
-        $('.question').html(question);
-        for (var i = 0; i < 4; i++) {
-            var answer = triviaGame[indexQandA].answer[i];
-            $('.answers').append('<h4 class= answersAll id=' + i + '>' + answer + '</h4>');
-        
-        }
-
-        $("h4").click(function () {
-            var id = $(this).attr('id');
-            if (id === correct) {
-                answered = true; // stops the timer
-                $('.question').html("the answer is: " + triviaGame[indexQandA].answer[correct]);
-                correctAnswer();
-                $('.question').remove();
-            } else {
-                answered = true; //stops the timer
-                $('.question').html("you picked: " + triviaGame[indexQandA].answer[id] + " BUT you should have picked: " + triviaGame[indexQandA].answer[correct]);
-                incorrectAnswer();
-                setTimeout( "$('.question').hide();", 2000);
-            }
-
-            // function clear(){
-            //     $('.question').text("");
-            // }
-           
-        });
-    }
-
-    function timer() {
-        if (timeRemaining === 0) {
-            answered = true;
-            clearInterval(intervalID);
-            $('.question').html("the correct answer is: " + triviaGame[indexQandA].answer[correct]);
-            unAnswered();
-        } else if (answered === true) {
-            clearInterval(intervalID);
-        } else {
-            timeRemaining--;
-            $('.timeRemaining').html('you only have ' + timeRemaining + ' seconds to pick!');
-        }
-    }
-
-    function correctAnswer() {
-        correctAnswers++;
-        $('.timeRemaining').html("you are right!").css({
-            'color': '#3D414F'
-        });
-        resetRound();
-    }
-
-    function incorrectAnswer() {
-        incorrectAnswers++;
-        $('.timeRemaining').html("you are wrong!").css({
-            'color': '#3D414F'
-        });
-        resetRound();
-    }
-
-    function unAnswered() {
-        unansweredQuestions++;
-        $('.timeRemaining').html("you didnt pick an answer?!").css({
-            'color': '#3D414F'
-        });
-        resetRound();
-    }
-
-    function resetRound() {
-        $('.answersAll').remove();
-        indexQandA++; // increments index which will load next question when loadQandA() is called again
-        if (indexQandA < triviaGame.length) {
-            setTimeout(function () {
-                loadQandA();
-            }, 5000); 
-        } else {
-            setTimeout(function () {
-                $('.question').remove();
-                $('.timeRemaining').remove();
-                $('.answers').append('<h4 class= answersAll end>correct: ' + correctAnswers + '</h4>');
-                $('.answers').append('<h4 class= answersAll end>incorrect: ' + incorrectAnswers + '</h4>');
-                $('.answers').append('<h4 class= answersAll end>unanswered: ' + unansweredQuestions + '</h4>');
-                setTimeout(function () {
-                    location.reload();
-                }, 7000);
-            }, 5000);
-        }
-    };
-
-    $('.startButton').on("click", function () {
-        $('.startButton');
-        startGame();
-
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    // sort highscores by score property in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
     });
-
-});
+    highscores.forEach(function(score) {
+      // create li tag for each high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+      // display on page
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  document.getElementById("clear").onclick = clearHighscores;
+  // run function when page loads
+  printHighscores();
