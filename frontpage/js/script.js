d6b052a755489157393f179a01487bbf0aef7117
Brooke Kumar <mrsbrookekumar@gmail.com>
2019-11-08 22:42:58 -0600

    finalized all styling to align with brand story

---
+++ frontpage/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+var submit=document.getElementById("submitButton");
+submitButton.addEventListener("click",sendMail(),false); 
+
+function sendMail() {
+
+    window.open('mailto:mrsbrookekumar@gmail.com');
+
+}
